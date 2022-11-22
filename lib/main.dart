ce9178cf25506772b7420ac6fa446667b6630d28
Om Bhatt <ombhatt1158@gmail.com>
2022-11-22 23:54:53 +0530

    Vx flutter addon

--- lib/main.dart
+++ lib/main.dart
@@ -7,26 +7,25 @@ import 'package:flutter_application_2/utils/routes.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-
       themeMode: ThemeMode.light,
       theme: MyTheme.lightTheme(context),
       darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
+
       initialRoute: MyRoutes.homeRoute,
       routes: {
         "/": ((context) => const Login()),
-        MyRoutes.homeRoute: ((context) => const Home()),
+        MyRoutes.homeRoute: ((context) => const HomePage()),
         MyRoutes.loginRoute: ((context) => const Login())
       },
     );
   }
-
   bringVegetables({required bool thaila, int rupees = 100}) {}
 }
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -4,16 +4,18 @@ import 'package:google_fonts/google_fonts.dart';
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
       primarySwatch: Colors.deepPurple,
-      fontFamily: GoogleFonts.lato().fontFamily,
+      fontFamily: GoogleFonts.poppins().fontFamily,
       appBarTheme: AppBarTheme(
         color: Colors.white,
         elevation: 0.0,
-        iconTheme: const IconThemeData(color: Color.fromARGB(255, 0, 0, 0)),
-        // ignore: deprecated_member_use
-        textTheme: Theme.of(context).textTheme,
+        iconTheme: const IconThemeData(color: Colors.black), toolbarTextStyle: Theme.of(context).textTheme.bodyText2, titleTextStyle: Theme.of(context).textTheme.headline6,
       ));
 
   static ThemeData darkTheme(BuildContext context) => ThemeData(
         brightness: Brightness.dark,
       );
+
+  //Colors
+  static Color creamColor = const Color(0xfff5f5f5);
+  static Color darkBluishColor = const Color(0xff403b58);
 }
